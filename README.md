80f74600b3d27ae2e8a7243fb64b9e63eb439c0a
Michael Bayne <mdb@samskivert.com>
2014-04-30 08:26:48 -0700

    Basic XML editing mode.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scaled XML Mode
+
+A [Scaled] major editing mode for editing XML files.
+
+Documentation TBD.
+
+## Distribution
+
+Scaled XML Mode is released under the New BSD License. The most recent version of the code is
+available at http://github.com/scaled/xml-mode
+
+[Scaled]: https://github.com/scaled/scaled
---
+++ src/main/scala/scaled/xml/XmlMode.scala
@@ -0,0 +1,47 @@
+//
+// Scaled XML Mode - a Scaled major mode for editing XML files
+// http://github.com/scaled/xml-mode/blob/master/LICENSE
+
+package scaled.xml
+
+import scaled._
+import scaled.grammar._
+import scaled.major.CodeConfig
+
+object XmlConfig extends Config.Defs {
+  import EditorConfig._
+  import CodeConfig._
+  import GrammarConfig._
+
+  // maps TextMate grammar scopes to Scaled style definitions
+  val effacers = List(
+    effacer("comment.line", commentStyle),
+    effacer("comment.block", docStyle),
+    effacer("constant", constantStyle),
+    effacer("invalid", warnStyle),
+    effacer("keyword", keywordStyle),
+    effacer("string", stringStyle),
+
+    effacer("entity.name.tag", functionStyle),
+    effacer("entity.other", variableStyle),
+
+    effacer("variable.language.documentroot", preprocessorStyle),
+    effacer("variable.language.entity", typeStyle)
+  )
+
+  def xmlGrammar = Grammar.parse(stream("XML.tmLanguage"))
+  lazy val grammars = Seq(xmlGrammar)
+}
+
+@Major(name="xml",
+       tags=Array("project", "xml"),
+       pats=Array(".*\\.xml", ".*\\.tmLanguage"),
+       desc="A major mode for editing XML files.")
+class XmlMode (env :Env) extends GrammarCodeMode(env) {
+
+  override def dispose () {} // nada for now
+
+  override def configDefs = XmlConfig :: super.configDefs
+  override protected def grammars = XmlConfig.grammars
+  override protected def effacers = XmlConfig.effacers
+}
